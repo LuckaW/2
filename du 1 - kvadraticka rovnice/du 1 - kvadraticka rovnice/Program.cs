0b488cb3c7fbb669f0c45ee69a2e2db70d41e543
LuckaW <145652351+LuckaW@users.noreply.github.com>
2023-11-09 21:03:52 +0100

    Add files via upload

--- du 1 - kvadraticka rovnice/du 1 - kvadraticka rovnice/Program.cs
+++ du 1 - kvadraticka rovnice/du 1 - kvadraticka rovnice/Program.cs
@@ -1,9 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Tady se řeší kvadratická rovnice.");
 
-float a, b, c;
+float a, b, c, i, y;
 bool success;
+
+
+
 Console.WriteLine(" rovnice je a * x^2 + b * x + c = 0: ");
+
 do
 {
     Console.WriteLine(" Zadej a: ");
@@ -26,4 +32,29 @@ do
 
 } while (!success);
 
+float Sqrt(4 * a * c)
+{
 
+
+  
+    void prvnikrok(float[] a, float[] b, float[] c)
+    {
+        for (float i = ((-b) * Sqrt) / 2 * a)
+        {
+            prvnikrok([i]);
+        }
+
+    }
+
+
+    
+    void druhykrok(float[] a, float[] b, float[] c)
+    {
+        for (float y = ((b) * Sqrt) / 2 * a)
+        {
+            druhykrok([y]);
+        }
+
+    }
+}
+Console.WriteLine(" x1 je " + i ". x2 je " + y);
