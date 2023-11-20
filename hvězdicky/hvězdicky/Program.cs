9841d150c2603ece338d3b801d88c6bf37dee7d3
LuckaW <145652351+LuckaW@users.noreply.github.com>
2023-11-20 13:24:51 +0100

    Add files via upload

---
+++ hvězdicky/hvězdicky/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+    int a;
+    int b;
+    bool success;
+
+    do 
+    { 
+        Console.WriteLine("Napiš, kolik chceš SLOUPCŮ hvězdiček:");
+        string A = Console.ReadLine();
+        success = int.TryParse(A, out a);
+    } while (!success);
+
+    do
+    {
+        Console.WriteLine("Napiš, kolik chceš ŘÁDKŮ hvězdiček:");
+        string B = Console.ReadLine();
+        success = int.TryParse(B, out b);
+    } while (!success);
+
+for (int i = 0; i < b; i++)
+{
+
+    Console.Write("*");
+
+    for (int y = 1; y < a; y++)
+    {
+        Console.Write("*");
+
+    }
+    Console.WriteLine();
+}
