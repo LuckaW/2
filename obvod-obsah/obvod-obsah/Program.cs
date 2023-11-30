21703edf2553fbca8a1c138e06f8834e2c2f8c8f
LuckaW <145652351+LuckaW@users.noreply.github.com>
2023-11-30 08:03:05 +0100

    Add files via upload

--- obvod-obsah/obvod-obsah/Program.cs
+++ obvod-obsah/obvod-obsah/Program.cs
@@ -11,53 +11,53 @@ static void urcimoznost ( int možnost)
     {
         case 0:
             {
-              //  printctverec();
+                printctverec();
                 break;
             }
 
         case 1:
             {
-               // printobdelnik();
+                printobdelnik();
                 break;
             }
         case 2:
             {
-              //  printtrojuhelnik();
+                printtrojuhelnik();
                 break;
             }
         case 3:
             {
-                //printkruh();
+                printkruh();
                 break;
             }
         case 4:
             {
-               // printpetiuhelnik();
+                printpetiuhelnik();
                 break;
             }
         case 5:
             {
-               // printsestiuhelnik();
+                printsestiuhelnik();
                 break;
             }
         case 6:
             {
-               // printkrychle();
+                printkrychle();
                 break;
             }
         case 7:
             {
-                //printkvadr();
+                printkvadr();
                 break;
             }
         case 8:
             {
-                //printkuzel();
+                printkuzel();
                 break;
             }
         case 9:
             {
-               // printkoule();
+                printkoule();
                 break;
             }
         default: 
@@ -66,6 +66,10 @@ static void urcimoznost ( int možnost)
                 break;
             }
     }
+}
+int Index (string []moznost)
+{ 
+
 }
 Console.WriteLine("Vyber si obrazec a napiš jeho číslo:");
 foreach ( string obrazec in možnost) 
@@ -84,7 +88,91 @@ int vybralsi()
     return x;
 }
 urcimoznost(vybralsi());
+int zadejcislo()
+{
+    for (; ; )
+    {
+        Console.WriteLine("Zadej kladné číslo:");
+        
+        if(int.TryParse(string cislo = Console.ReadLine(),))
+        {
+            if (cislo > 0)
+            {
+                return cislo;
+            }
+        }
+    }
+}
 
 
+/*int a;
+int printctverec()
+{
+    Console.WriteLine("Zvolil jsi si čtverec.");
+    Console.WriteLine("Zadej délku strany 'a':")
+        int.TryParse(Console.ReadLine(), out a);
+    int obvod = a * 4;
+    int obsah = a * a;
+    Console.WriteLine("Obvod: " + obvod);
+    Console.WriteLine(¨"Obsah: " + obsah);
+
+}
+int b;
+int printobdelnik()
+{ 
+    Console.WriteLine("Zvolil jsi si obdélnik.");
+    Console.WriteLine("Zadej délku strany 'a':");
+    int.TryParse(Console.ReadLine(), out a);
+    Console.WriteLine("Zadej délku strany 'b':");
+    int.TryParse(Console.ReadLine(), out b);
+    int obvod = (2 * a) + (2 * b);
+    int obsah = a * b;
+    Console.WriteLine("Obvod: " + obvod);
+    Console.WriteLine(¨"Obsah: " + obsah);
+}
+int c;
+int printtrojuhelnik()
+{
+    Console.WriteLine("Zvolil jsi si trojúhelník.");
+    Console.WriteLine("Zadej délku strany 'a':");
+    int.TryParse(Console.ReadLine(), out a);
+    Console.WriteLine("Zadej délku strany 'b':");
+    int.TryParse(Console.ReadLine(), out b);
+    Console.WriteLine("Zadej délku strany 'c':");
+    int.TryParse(Console.ReadLine(), out c);
+    int obvod = a + b + c;
+    int obsah = (a * b) / 2;
+    Console.WriteLine("Obvod: " + obvod);
+    Console.WriteLine(¨"Obsah: " + obsah);
+}  
+int printkruh()
+{
+    Console.WriteLine("Zvolil jste si kruh.");
+}
+int printpetiuhelnik()
+       {
+    Console.WriteLine("Zvolil jste si pětiúhelník.");
+       }
+int printsestiuhelnik()
+       {
+        Console.WriteLine("Zvolil jste si šestiúhelník.");
+       }
+int printkrychle()
+       {
+        Console.WriteLine("Zvolil jste si krychli.");
+       }
+int printkvadr()
+       {
+        Console.WriteLine("Zvolil jste si kvádr.");
+}
+int printkuzel()
+       {
+        Console.WriteLine("Zvolil jste si kužel.");
+       }
+int printkoule()
+       {
+        Console.WriteLine("Zvolil jste si kouli.");
+       }
+*/
 
 
