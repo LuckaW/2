c534c701c9c651cdae098a87ceae67bc5e7785cc
LuckaW <145652351+LuckaW@users.noreply.github.com>
2023-12-04 13:36:43 +0100

    Add files via upload

---
+++ hadanka/hadanka/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+
+int x;
+Console.WriteLine("Co si myslím za číslo?(1-100)");
+
+
+int nejnizsi = 1;
+int nejvyssi = 100;
+Random cislo = new Random();
+int n = cislo.Next(nejnizsi, nejvyssi);
+do
+{
+    string X = Console.ReadLine();
+    int.TryParse(X, out x);
+    if (n < x)
+    {
+        Console.WriteLine("Číslo je nižší.");
+
+    }
+    else if (n > x)
+    {
+        Console.WriteLine("Číslo je vyšší.");
+    }
+    else
+    {
+        break;
+    }
+} while (x!=n);
+Console.WriteLine("Ano! Číslo je " + n);
