54e6204db4f288f5270d85c2ff3a0295d67752a3
LuckaW <145652351+LuckaW@users.noreply.github.com>
2023-12-15 10:36:55 +0100

    Add files via upload

---
+++ nakup jablek/nakup jablek/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int kg;
+bool success;
+do
+{
+    Console.WriteLine("Napište, kolik kilogramů jablek můžeme prodat:");
+    string KG = Console.ReadLine();
+    success = int.TryParse(KG, out kg);
+} while (!success);
+
+int vysledek;
+int jablko;
+do
+{
+    
+    do
+    {
+        Console.WriteLine("kolik kg si koupil zákazník?:");
+        string JABLKO = Console.ReadLine();
+        success = int.TryParse(JABLKO, out jablko);
+    } while (!success);
+   
+    if (kg > jablko)
+    {
+        kg = kg - jablko;
+        Console.WriteLine("Prodal/a/s mu jablka.");
+        Console.WriteLine("Přichází další zákazník...");
+    }
+    
+
+} while (kg !>= jablko);
+
+Console.WriteLine("Máš už jen " + kg + " kg jablek a zákazník chce " + jablko +  " kg jablek. Smůla.");
