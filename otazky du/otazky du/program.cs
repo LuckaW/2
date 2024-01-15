aa16ed6fc28b50aabd375cf2ab3eebebbec9ee85
LuckaW <145652351+LuckaW@users.noreply.github.com>
2024-01-15 15:21:38 +0100

    Add files via upload

--- otazky du/otazky du/program.cs
+++ otazky du/otazky du/program.cs
@@ -9,47 +9,83 @@ string odpoved()
 
 while (true)
 {
-    
-    string[] seznamek = File.ReadAllLines("questions_small.data");
+    float skóre = 0;
 
+    string[] seznamek = File.ReadAllLines("questions.data");
+
+    string[] dvojnik = { };
+
+    while (dvojnik.Length < seznamek.Length)
+    {
+        int nahoda = new Random().Next(seznamek.Length);
+        if (!dvojnik.Contains(seznamek[nahoda]))
+        {
+            Array.Resize(ref dvojnik, dvojnik.Length +1);
+            dvojnik[dvojnik.Length - 1] = seznamek[nahoda];
+        }
+    }
+    seznamek = dvojnik; 
 
     for (int bob = 0; bob < seznamek.Length; bob++)
     {
-
+        
         string[] cast = seznamek[bob].Split('|');
         Console.WriteLine(cast[0]);
-        Console.WriteLine("A " + cast[2]);
-        Console.WriteLine("B " + cast[3]);
-        Console.WriteLine("C " + cast[4]);
+
+        string[] okodpoved = {"A","B"};
+
+        for (int bobek = 2; bobek < cast.Length; bobek++)
+        {
+           
+            switch (bobek)
+            {
+                case 2:
+                    Console.WriteLine("A " + cast[2]);
+
+                    break;
+                case 3:
+                    Console.WriteLine("B " + cast[3]);
+                    break;
+                case 4:
+                    Console.WriteLine("C " + cast[4]);
+                    Array.Resize(ref okodpoved, okodpoved.Length + 1);
+                    okodpoved[okodpoved.Length - 1] += "C";
+                    break;
+
+            }
+        }
         Console.WriteLine("Napiste písmeno správné odpovědi:");
 
         string pismeno;
         while (true)
         {
             pismeno = odpoved();
-            if (pismeno == "A" || pismeno == "B" || pismeno == "C")
+            if (okodpoved.Contains(pismeno))
             {
+                
                 break;
             }
             else
             {
-                Console.WriteLine("To fakt není A/B/C... zkus znova.");
+                Console.WriteLine("To fakt není písmeno odpovědi... zkus znova.");
             }
         }
 
         if (cast[1] == pismeno)
         {
             Console.WriteLine("Spravna odpověď. Bravo. :D");
+            skóre ++;
         }
         else
         {
-            Console.WriteLine("Špatná odpověď. Smůla... :(");
+            Console.WriteLine("Špatná odpověď. Smůla... :(, Správná odpověď je " + cast[1]);
         }
         Console.WriteLine();
     }
 
 
-    Console.WriteLine("Konec kvízu.");
+    Console.WriteLine("Konec kvízu. Tvoje skóre bylo " + MathF.Round(skóre / seznamek.Length * 100 ) + "%.");
+
     while (true)
     {
         Console.WriteLine("Pokud nechceš opakovat program, napiš 'q':");
