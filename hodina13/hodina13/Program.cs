29a151bb698c3242756423ede4b6e1afacb36bb9
LuckaW <145652351+LuckaW@users.noreply.github.com>
2024-02-01 09:41:55 +0100

    Add files via upload

---
+++ hodina13/hodina13/Program.cs
@@ -0,0 +1,9 @@
+﻿person person = new person();
+person.NumFingers = 1;
+person.Adress = "Spitalska 2";
+person.height = 1.80f;
+
+person person02 = new person();
+
+person[] people = new person[4];
+person.Printperson();
---
+++ hodina13/hodina13/člověk.cs
@@ -0,0 +1,11 @@
+﻿public class person
+{
+    public string Adress;
+    public int NumFingers;
+    public float height;
+    public void Printperson()
+    {
+        Console.WriteLine(Adress);
+    }
+
+}
