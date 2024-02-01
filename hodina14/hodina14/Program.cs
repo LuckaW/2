27b04b5f47b2f974fd9cfac67feb7a2296c08ebb
LuckaW <145652351+LuckaW@users.noreply.github.com>
2024-02-01 09:41:13 +0100

    Add files via upload

---
+++ hodina14/hodina14/Program.cs
@@ -0,0 +1,10 @@
+﻿animal zvire = new animal("jmeno", "typ");
+
+zvire.Name = "Alex";
+zvire.Type = "kocka";
+zvire.pozdrav();
+//zvire.promluv();
+zvire.mluv();
+bird Pepa = new bird("Pepa", "bird");
+Pepa.mluv();
+Pepa.pozdrav();
---
+++ hodina14/hodina14/zvíře.cs
@@ -0,0 +1,49 @@
+﻿
+
+public class animal
+{
+    public string Name;
+    public string Type;
+    
+    public void pozdrav()
+    {
+        Console.WriteLine("Hellooo " + Name);
+    }
+   /* public void promluv()
+    {
+        Console.WriteLine("Co chceš, aby " + Name + " řekl?");
+        string blabla = Console.ReadLine();
+        Console.WriteLine(Name + ": " + blabla);
+    }*/
+   
+    public void mluv()
+    {
+       
+        if (Type == "pes")
+        {
+            Console.WriteLine("Haf haf");
+        }
+        if (Type == "kocka")
+        {
+            Console.WriteLine("Mňaaauuuu");
+        }
+    }
+    public animal (string jmeno, string typ)
+    {
+        Name = jmeno;
+        Type = typ;
+    }
+    
+}
+
+public class bird : animal
+{
+    public bird(string jmeno, string typ) : base(jmeno, typ)
+    {
+        
+    }
+    public void mluv()
+    {
+        Console.WriteLine("Píp píp");
+    }
+}
