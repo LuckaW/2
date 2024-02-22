f48535f81a0a351e604840aaee0b7f3500ad29bf
LuckaW <145652351+LuckaW@users.noreply.github.com>
2024-02-22 09:42:48 +0100

    Add files via upload

---
+++ hodina 16/hodina 16/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Datové struktury
+         pole - array
+         spojový seznam - linked list
+         strom - tree
+         graf - graph
+dnes   zásobník - stack, píše se: LIFO (last in first out)
+            - funkce:
+                add() - přidat
+                set() - odebrat
+                pop() - posunu
+dnes   fronta - queue, píše se: FIFO (first in, first out)
+           
+*/
+
+//udělat: stack - capacity + array + last + set() + add()
+int neco;
+Console.WriteLine("Něco přidej:");
+string něco = Console.ReadLine();
+int.TryParse(něco, out neco);
+
+Stack zasobnik = new Stack (10);
+zasobnik.Add(neco);
---
+++ hodina 16/hodina 16/Stack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Stack
+{
+    int[] array;
+    int posledni;
+    
+    public Stack(int capacity) 
+    {
+        array = new int[capacity];
+    }
+    public int Get()
+    {
+        int cistvalue = array[posledni];
+        return cistvalue;
+    }
+
+    public int Add(int value)
+    {
+        array[posledni + 1] = value;
+        posledni = posledni + 1;
+        return posledni;
+    }
+
+}
